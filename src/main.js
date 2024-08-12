eb37b539f0199c04deeaf42760785387c03ca2cc
MatseVH <49431416+Matse2005@users.noreply.github.com>
2024-08-12 16:34:08 +0200

    Moved to vite

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("node:path");
-const { updateElectronApp } = require("update-electron-app");
-updateElectronApp({
-  notifyUser: true,
-  updateInterval: "5 minutes",
-});
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
@@ -21,11 +16,17 @@ const createWindow = () => {
     },
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "index.html"));
+  mainWindow.maximize();
+  mainWindow.setMenu(null);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // and load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+    );
+  }
 };
 
 // This method will be called when Electron has finished
